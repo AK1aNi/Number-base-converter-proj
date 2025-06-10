41a32a99aafaa34b1268d2ec76b56fbb5c6aa3da
Muhammad Abdullah Kiani <22MKiani@heckgrammar.co.uk>
2025-06-10 09:40:58 +0100

    kikpk

--- Program.cs
+++ Program.cs
@@ -16,8 +16,84 @@
                     binary = Convert.ToString(d_num % 2) + binary;
                     d_num = d_num / 2;
                 }
-                Console.WriteLine(binary);
+
+                Console.WriteLine(d_num+" converted to binary is "+binary);
             }
+            string hex = "";
+            if (base_n == 16)
+            {
+                int first_p = d_num / 16;
+                int remain_p = d_num % 16;
+
+                if (first_p < 10)
+                {
+                    hex = first_p + hex;
+                }
+                else
+                {
+                    if (first_p == 10)
+                    {
+                        hex = "A" + hex;
+                    }
+                    if (first_p == 11)
+                    {
+                        hex = "B" + hex;
+                    }
+                    if (first_p == 12)
+                    {
+                        hex = "C" + hex;
+                    }
+                    if (first_p == 13)
+                    {
+                        hex = "D" + hex;
+                    }
+                    if (first_p == 14)
+                    {
+                        hex = "E" + hex;
+                    }
+                    if (first_p == 15)
+                    {
+                        hex = "F" + hex;
+                    }
+
+                }
+
+                if (remain_p < 10)
+                {
+                    hex = hex + remain_p;
+                }
+                else
+                {
+                    if (remain_p == 10)
+                    {
+                        hex = hex + "A";
+                    }
+                    if (remain_p == 11)
+                    {
+                        hex = hex + "B";
+                    }
+                    if (remain_p == 12)
+                    {
+                        hex = hex + "C";
+                    }
+                    if (remain_p == 13)
+                    {
+                        hex = hex + "D";
+                    }
+                    if (remain_p == 14)
+                    {
+                        hex = hex + "E";
+                    }
+                    if (remain_p == 15)
+                    {
+                        hex = hex + "F";
+                    }
+
+                }
+
+                Console.WriteLine(d_num + "converted to hexdecimal is "+hex);
+            }
+
         }
     }
 }
